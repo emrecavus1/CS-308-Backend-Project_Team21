b778c372f83cdd537da2ac6bd2a9fb8d457cc503
emrecavus1 <emre.cavus@sabanciuniv.edu>
2025-03-07 20:16:49 +0300

    Delete Back-end directory

--- Back-end/src/main/java/com/cs308/backend/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cs308.backend.repositories;
-
-import com.cs308.backend.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByEmail(String email); // ✅ Added this method
-}
