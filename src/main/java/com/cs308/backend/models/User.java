a3b21e6b1ff1dfb2234ad2d2665105a070deea25
Emre Çavuş <emrecavus@Emres-MacBook-Pro.local>
2025-03-07 19:41:38 +0300

    Initial Commit

---
+++ src/main/java/com/cs308/backend/models/User.java
@@ -0,0 +1,36 @@
+package com.cs308.backend.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+    private String role;
+    private String city;
+    private String phoneNumber;
+    private String specificAddress;
+
+    public User(String email, String password, String name, String surname, String role, String specificAddress, String city, String phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.role = role;
+        this.specificAddress = specificAddress;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+    }
+}
