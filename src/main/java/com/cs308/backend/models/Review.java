02f7333759dd5c80c1c32a9baa50b88d748f29e0
Denizhan Altan <denizhanaltan34@gmail.com>
2025-03-17 22:26:38 +0300

    "New Models Added"

---
+++ src/main/java/com/cs308/backend/models/Review.java
@@ -0,0 +1,21 @@
+package com.cs308.backend.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.cs308.backend.models.*;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "reviews")
+public class Review {
+    @Id
+    private double rating;
+    private String comment;
+    private boolean isVerified;
+    // There will be a user id here (foreign key) //
+
+}
